317e45fd487bafa92a252ab32260f5c93db29080
imanakasan181 <2370526h@stu.kobe-u.ac.jp>
2024-10-08 12:07:13 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -2,5 +2,23 @@
 function setup(){
   for(let i = 0; i < 10; i++){
     // BLANK[1]
+    stroke(255, 0, 0);
+    ellipse(50, 50, 99);
+    ellipse(50, 50, 90);
+    ellipse(50, 50, 80);
+    ellipse(50, 50, 70);
+    ellipse(50, 50, 60);
+    stroke(0, 0, 255);
+    ellipse(50, 50, 50);
+    ellipse(50, 50, 40);
+    ellipse(50, 50, 30);
+    ellipse(50, 50, 20);
+    ellipse(50, 50, 10);}
   }
-}
+
+
+  
+  
+
+  
+  
